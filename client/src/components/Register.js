fa8d11acebb78bc200c35abfab99cc731b89384a
Franco Speziali <franco.speziali@digitalcareerinstitute.org>
2022-04-29 16:57:26 +0200

    Updated npm, gitignore and removed build folder

--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -1,47 +1,47 @@
 export default function Register() {
-    const handleSubmit = async (event) => {
-        event.preventDefault();
-        const formData = new FormData(event.target);
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const formData = new FormData(event.target);
 
-        const data = {
-            email: formData.get("email"),
-            name: formData.get("name"),
-            password: formData.get("password"),
-        };
-
-        try {
-            await fetch("", {});
-        } catch (error) {
-            console.log(error);
-        }
+    const data = {
+      email: formData.get("email"),
+      name: formData.get("name"),
+      password: formData.get("password"),
     };
 
-    return (
-        <>
-            <h1>Register</h1>
-            <form onSubmit={handleSubmit}>
-                <label>
-                    Username
-                    <input name="username" type="text" required={true} />
-                </label>
-                <label>
-                    First Name
-                    <input name="firstname" type="text" required={true} />
-                </label>
-                <label>
-                    Last Name
-                    <input name="lastname" type="text" required={true} />
-                </label>
-                <label>
-                    Email
-                    <input name="email" type="email" required={true} />
-                </label>
-                <label>
-                    Password
-                    <input name="password" type="password" required={true} />
-                </label>
-                <button>Register</button>
-            </form>
-        </>
-    );
+    try {
+      await fetch("", {});
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <>
+      <h1>Register</h1>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Username
+          <input name="username" type="text" required={true} />
+        </label>
+        <label>
+          First Name
+          <input name="firstname" type="text" required={true} />
+        </label>
+        <label>
+          Last Name
+          <input name="lastname" type="text" required={true} />
+        </label>
+        <label>
+          Email
+          <input name="email" type="email" required={true} />
+        </label>
+        <label>
+          Password
+          <input name="password" type="password" required={true} />
+        </label>
+        <button>Register</button>
+      </form>
+    </>
+  );
 }
