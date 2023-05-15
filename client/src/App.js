fe3d3744360d03c6acd593554540fc0441b26ca8
Franco Speziali <franco.speziali@digitalcareerinstitute.org>
2023-05-15 16:44:16 +0200

    Updated React

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { AppBar, Box, Button } from "@mui/material";
-import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+import { Link, Route, Routes } from "react-router-dom";
 import "./App.css";
 
 import Register from "./components/Register";
@@ -9,22 +9,20 @@ import Login from "./components/Login";
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-        <Box sx={{ flexGrow: 1 }}>
-          <AppBar position="static">
-            <Button color="inherit" component={Link} to={"/login"}>
-              Login
-            </Button>
-            <Button color="inherit" component={Link} to={"/register"}>
-              Register
-            </Button>
-          </AppBar>
-        </Box>
-        <Routes>
-          <Route path="/register" element={<Register />} />
-          <Route path="/login" element={<Login />} />
-        </Routes>
-      </BrowserRouter>
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="static">
+          <Button color="inherit" component={Link} to={"/login"}>
+            Login
+          </Button>
+          <Button color="inherit" component={Link} to={"/register"}>
+            Register
+          </Button>
+        </AppBar>
+      </Box>
+      <Routes>
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
     </div>
   );
 }
